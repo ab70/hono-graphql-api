d1156e73f987592ccbef6e92f7cf290243ed3952
ab70 <nurulabrar2369@gmail.com>
2024-05-30 01:13:01 +0600

    login mutation

--- src/graphql/resolvers/auth/mutations.ts
+++ src/graphql/resolvers/auth/mutations.ts
@@ -2,29 +2,29 @@
 
 import { YogaInitialContext } from "graphql-yoga";
 import { createUser, getUserByCredentials } from "./functions/authFunctions";
-import { useCookies } from '@whatwg-node/server-plugin-cookies'
+import { useCookies } from "@whatwg-node/server-plugin-cookies";
 
 const authMutations = {
   signup: async (_, { username, password }, ctx: YogaInitialContext) => {
-
     const newUser = await createUser({ username, password });
-    await ctx.request.cookieStore?.set("hello", "tello")
+    await ctx.request.cookieStore?.set("hello", "tello");
     // console.log("con", honoContext.c);
 
     // await setSignedCookie(honoContext.c, "secret", "jjkl", "jjkloi", { httpOnly: false, path: "/", secure: false, signingSecret: "JWT_SECRET", sameSite: "Lax" })
 
     return { username, password };
   },
-  login: async (_, { username, password }) => {
+  login: async (_, { username, password }, ctx: YogaInitialContext) => {
     const user = await getUserByCredentials({ username, password });
-    // if (user) {
-    //   // Generate and return JWT token or other authentication identifier
-    //   // Replace with your actual token generation logic
-    //   const token = 'your_generated_token';
-    //   return token;
-    // } else {
-    //   return null; // Indicate failed login
-    // }
+    if (user) {
+      // Generate and return JWT token or other authentication identifier
+      // Replace with your actual token generation logic
+
+      await ctx.request.cookieStore?.set("hello", "tello");
+      return user;
+    } else {
+      return null; // Indicate failed login
+    }
   },
 };
 export default authMutations;
